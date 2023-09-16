9941583717bfcb22aa2dcea57fa8e7ef7ea416a9
Kento210 <T121015@ed.sus.ac.jp>
2023-09-16 19:49:36 +0000

    UserInfoコンポーネントを作った

---
+++ suwaaa-app/components/UserInfo.tsx
@@ -0,0 +1,28 @@
+// components/UserInfoBar.tsx
+import React from 'react';
+import styles from './UserInfoBar.module.css';
+import { Button } from '@mui/material';
+
+interface UserInfoBarProps {
+  userName: string;
+  userId: string;
+  onSettingClick: () => void;
+}
+
+const UserInfoBar: React.FC<UserInfoBarProps> = ({ userName, userId, onSettingClick }) => {
+  return (
+    <div className={styles.userInfoBar}>
+      <div className={styles.userInfo}>
+        <div>{userName}</div>
+        <div>Suwaaa ID: {userId}</div>
+      </div>
+      <div className={styles.settingButton}>
+        <Button variant="outlined" onClick={onSettingClick}>
+          設定
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default UserInfoBar;
