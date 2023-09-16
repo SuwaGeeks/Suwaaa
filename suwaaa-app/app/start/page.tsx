acc1873a195ea4aefb059a2a41d41fac8cde1170
Kento210 <T121015@ed.sus.ac.jp>
2023-09-16 07:48:40 +0000

    背景とボタンの位置調節

--- suwaaa-app/app/start/page.tsx
+++ suwaaa-app/app/start/page.tsx
@@ -4,30 +4,30 @@ import Link from 'next/link';
 import styles from './page.module.css';
 
 export default function Start() {
-    return (
-      <Container className={styles.container}>
-        <div className={styles.imageContainer}>
-          {/* 背景画像 */}
-        </div>
-        <div className={styles.buttonContainer}>
-          <Link href="/signup" passHref>
-            <Button variant="contained" style={{ backgroundColor: '#217178', color: 'white' }}>
-              はじめて利用する
-            </Button>
-          </Link>
-          <Button variant="contained" style={{ backgroundColor: '#D9D9D9', color: 'black' }}>
-            アカウントを持っている
+  return (
+    <Container className={styles.container}>
+      <img src="/image_green.png" alt="Background" className={styles.backgroundImage} />
+      <img src="/Suwaaa-logo.png" alt="Suwaaa Logo" className={styles.logo} />
+      <div className={styles.buttonContainer}>
+        <Link href="/signup" passHref>
+          <Button variant="contained" style={{ backgroundColor: '#217178', color: 'white' }}>
+            はじめて利用する
           </Button>
-        </div>
-        <Typography variant="body2" className={styles.footer}>
-          本サービスの利用開始により、
-          <Link href="/start/policy" passHref>
-            <Typography component="span" variant="body2" style={{ textDecoration: 'underline', cursor: 'pointer' }}>
-              利用規約
-            </Typography>
-          </Link>
-          とプライバシーポリシーに同意したことになります。
-        </Typography>
-      </Container>
-    );
-  }
+        </Link>
+        <Button variant="contained" style={{ backgroundColor: '#D9D9D9', color: 'black' }}>
+          アカウントを持っている
+        </Button>
+      </div>
+      <Typography variant="body2" className={styles.footer}>
+        本サービスの利用開始により、
+        <Link href="/start/policy" passHref>
+          <Typography component="span" variant="body2" style={{ textDecoration: 'underline', cursor: 'pointer' }}>
+            利用規約
+          </Typography>
+        </Link>
+        とプライバシーポリシーに同意したことになります。
+      </Typography>
+    </Container>
+  );
+}
+
