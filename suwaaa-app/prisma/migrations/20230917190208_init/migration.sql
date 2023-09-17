1068fe3a1c23767e81ac33a22b8570264b34262c
Sora-210 <sokominto1231@gmail.com>
2023-09-17 19:13:52 +0000

    update: マイグレーションの統一

--- suwaaa-app/prisma/migrations/20230917190208_init/migration.sql
+++ suwaaa-app/prisma/migrations/20230917190208_init/migration.sql
@@ -11,12 +11,14 @@ CREATE TABLE `Users` (
 
 -- CreateTable
 CREATE TABLE `Auth` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
+    `schoolId` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `isValid` BOOLEAN NOT NULL DEFAULT false,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`email`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
@@ -73,6 +75,17 @@ CREATE TABLE `Benefits` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `BenefitHistory` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `benefitId` INTEGER NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `Auth` ADD CONSTRAINT `Auth_email_fkey` FOREIGN KEY (`email`) REFERENCES `Users`(`email`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -87,3 +100,9 @@ ALTER TABLE `Shops` ADD CONSTRAINT `Shops_genreId_fkey` FOREIGN KEY (`genreId`) 
 
 -- AddForeignKey
 ALTER TABLE `Benefits` ADD CONSTRAINT `Benefits_shopId_fkey` FOREIGN KEY (`shopId`) REFERENCES `Shops`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `BenefitHistory` ADD CONSTRAINT `BenefitHistory_benefitId_fkey` FOREIGN KEY (`benefitId`) REFERENCES `Benefits`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `BenefitHistory` ADD CONSTRAINT `BenefitHistory_email_fkey` FOREIGN KEY (`email`) REFERENCES `Users`(`email`) ON DELETE RESTRICT ON UPDATE CASCADE;
