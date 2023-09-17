f3809dcc45aa80e9019962a1588b8e744514d5b1
Sora-210 <sokominto1231@gmail.com>
2023-09-17 19:26:00 +0000

    fix: authテーブルのprimaryを変更

--- suwaaa-app/prisma/migrations/20230917192414_init/migration.sql
+++ suwaaa-app/prisma/migrations/20230917192414_init/migration.sql
@@ -11,6 +11,7 @@ CREATE TABLE `Users` (
 
 -- CreateTable
 CREATE TABLE `Auth` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
     `schoolId` VARCHAR(191) NOT NULL,
     `token` VARCHAR(191) NOT NULL,
@@ -18,7 +19,7 @@ CREATE TABLE `Auth` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    PRIMARY KEY (`email`)
+    PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
