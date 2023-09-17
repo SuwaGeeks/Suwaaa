94748171c9187c36f34bd21f5f38df91ca00a52d
Sora-210 <sokominto1231@gmail.com>
2023-09-17 12:55:47 +0000

    add: ログイン関係のapi作成

---
+++ suwaaa-app/app/api/login/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server'
+import { PrismaClient } from '@prisma/client'
+import { createToken } from '../utils/jwt'
+import dayjs from 'dayjs'
+
+const prisma = new PrismaClient()
+
+export async function POST(request: Request) {
+    const { email, password } = await request.json()
+    const cUser = await prisma.users.count({
+        where: { email, password }
+    })
+
+    if (!cUser) {
+        return NextResponse.json(
+            {
+                status: "Error",
+                message: "Credentials are different"
+            },
+            {
+                status: 403
+            }
+        )
+    }
+
+    const token = createToken(email)
+
+    return NextResponse.json(
+        {
+            token,
+            exp: dayjs().add(30, 'day').toISOString()
+        }
+    )
+}
