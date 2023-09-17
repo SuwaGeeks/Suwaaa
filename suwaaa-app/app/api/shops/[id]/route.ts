4b9c5e72e417fe7bd8fc776bf8b1ecc1016dcd75
Sora-210 <sokominto1231@gmail.com>
2023-09-17 14:45:44 +0000

    add: shop関連のAPI

---
+++ suwaaa-app/app/api/shops/[id]/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+type Props = {
+    params: {
+        id: string;
+    }
+}
+
+export async function GET(request: Request, params: Props) {
+    console.log(params)
+    
+    const shop = await prisma.shops.findFirst({
+        where: {
+            id: Number(params.params.id)
+        }
+    })
+
+    if (shop) {
+        return NextResponse.json(shop)
+    } else {
+        return NextResponse.json(
+            {
+                status: "Error",
+                message: "NotFound"
+            },
+            {
+                status: 404
+            }
+        )
+    }
+}
