1be43b36e6df483da1ec0f664bb6207c90795f8a
Sora-210 <sokominto1231@gmail.com>
2023-09-18 14:22:58 +0000

    change: デザインの一部変更

--- suwaaa-app/app/terms/page.tsx
+++ suwaaa-app/app/terms/page.tsx
@@ -1,15 +1,15 @@
 // start/policy/page.tsx
-import TopBar from '../../components/TopBar';
+import { Button } from '@mui/material'
 import styles from './page.module.css';
 
 export default function Policy() {
   return (
     <div className={styles.container}>
-      <TopBar goBackTo="/start" pageTitle="利用規約" />
-      <br></br>
       <h1>利用規約</h1>
+      <h2>第一条</h2>
       <p>ここに利用規約の内容を記述します。</p>
-      {/* 何か追加するものがあればここに */}
+      <h2>第二条</h2>
+      <p>ここに利用規約の内容を記述します。</p>
     </div>
   );
 }
