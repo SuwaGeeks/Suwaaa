63a6cde308ef52bee9c489f7e8c2eb24bb6f4fd8
Sora-210 <sokominto1231@gmail.com>
2023-10-04 07:28:29 +0000

    fix: mergeミス

--- suwaaa-app/app/page.tsx
+++ suwaaa-app/app/page.tsx
@@ -48,7 +48,7 @@ export default function Start() {
       </div>
       <Typography variant="body2" className={styles.footer}>
         本サービスの利用開始により、
-        <Link href="/start/policy" passHref>
+        <Link href="/terms" passHref>
           <Typography
             component="span"
             variant="body2"
